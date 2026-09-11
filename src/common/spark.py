dd27677dad391d4f6d898eab50e0cd6a3da3a072
Harshadx7 <130553782+Harshadx7@users.noreply.github.com>
2026-09-11 06:30:24 +0530

    updated

--- src/common/spark.py
+++ src/common/spark.py
@@ -1,6 +1,23 @@
+from delta import configure_spark_with_delta_pip
 from pyspark.sql import SparkSession
+
+
 def get_spark(app_name):
-    return (SparkSession.builder.appName(app_name)
-        .config("spark.sql.extensions","io.delta.sql.DeltaSparkSessionExtension")
-        .config("spark.sql.catalog.spark_catalog","org.apache.spark.sql.delta.catalog.DeltaCatalog")
-        .getOrCreate())
+
+    builder = (
+        SparkSession.builder
+        .appName(app_name)
+        .config(
+            "spark.sql.extensions",
+            "io.delta.sql.DeltaSparkSessionExtension",
+        )
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
+    )
+
+    return (
+        configure_spark_with_delta_pip(builder)
+        .getOrCreate()
+    )
